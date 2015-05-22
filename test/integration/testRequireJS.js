54e4a9effaf0d6b4f054fb00a8fddfba3e2d8bed
magska <magnus.skaalsveen@netlight.com>
2015-05-22 14:14:07 +0200

    require should be on top

--- test/integration/testRequireJS.js
+++ test/integration/testRequireJS.js
@@ -3,32 +3,33 @@
 
 'use strict';
 
+var chai = require('chai');
+
 module.exports = {
-    'Pageload with requirejs': function (browser) {
-        var chai = require('chai');
-        browser
-        .deleteCookies()
-        .getCookies(function callback(result) {
-            this.assert.equal(result.value.length, 0);
-        })
+	'Pageload with requirejs': function (browser) {
+		browser
+		.deleteCookies()
+		.getCookies(function callback(result) {
+			this.assert.equal(result.value.length, 0);
+		})
 		.url('http://127.0.0.1:8080/dev/requiretests/indexSimple.html')
-        .waitForElementVisible('body', 1000)
+		.waitForElementVisible('body', 1000)
 		.assert.elementPresent('#require-script')
 		.pause(1000)
 		.execute(function() {
-            return server.requests;
-        }, [''], function(res) {
+			return server.requests;
+		}, [''], function(res) {
 			this.assert.equal(res.value.length, 2);
 			var requestBody = JSON.parse(res.value[1].requestBody);
 			chai.assert.include(requestBody[0].provider['@id'], 'pulseTest');
-        })
+		})
 		.execute(function() {
 			return AutoTrack.getVisitorId();
 		}, [''], function(res) {
 			this.assert.equal(res.value, 'abcd3456');
 		})
 		.getCookies(function callback(result) {
-            this.assert.equal(result.value.length, 4);
+			this.assert.equal(result.value.length, 4);
 
 			var nameArray = [
 				'_DataTrackerSession',
